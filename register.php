4081f12dd9d7c820d14423f9ac517f5fa1e17892
BAHMAN <bahmansnights@gmail.com>
2025-07-14 16:53:32 +0430

    insert data from register.php

--- register.php
+++ register.php
@@ -1,6 +1,41 @@
 <?php
 include("Database/PDO-Connection.php");
+$error = "";
+
+if (isset($_POST['sub'])) {
+    if (
+        isset($_POST['username']) && $_POST['username'] !== '' &&
+        isset($_POST['email']) && $_POST['email'] !== '' &&
+        isset($_POST['password']) && $_POST['password'] !== '' &&
+        isset($_POST['repassword']) && $_POST['repassword'] !== ''
+    ) {
+        if ($_POST['password'] !== $_POST['repassword']) {
+            $error = "رمز عبور با تکرار آن یکسان نیست";
+        } elseif (strlen($_POST['password']) < 5) {
+            $error = "رمز عبور باید بیشتر از 5 کاراکتر باشد";
+        } else {
+            // رمزها درست هستند و طول مناسبه، حالا ذخیره کن
+            $username = $_POST['username'];
+            $email = $_POST['email'];
+            $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+            $result = $connection->prepare("INSERT INTO users (username, email, password) VALUES (?, ?, ?)");
+            $result->bindValue(1, $username);
+            $result->bindValue(2, $email);
+            $result->bindValue(3, $password);
+
+            if ($result->execute()) {
+                echo "ثبت‌نام با موفقیت انجام شد.";
+            } else {
+                $error = "خطا در ثبت اطلاعات.";
+            }
+        }
+    } else {
+        $error = "لطفاً تمام فیلدها را پر کنید.";
+    }
+}
 ?>
+
 
 
 <!DOCTYPE html>
@@ -18,49 +53,52 @@ include("Database/PDO-Connection.php");
     <!-- Vazir Font -->
     <link rel="stylesheet" href="fonts/vazir.css">
     <!-- Fontawsome CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <title>ثبت نام</title>
 </head>
 <body>
-    <section class="d-flex justify-content-center align-items-center min-h-screen bg">
-        <div id="overlay"></div>
-        <div class="form-container">
-            <form action="#">
-                <h1 class="title">ثبت نام در وبلاگ</h1>
-                <div class="mt-3 position-relative">
-                    <input type="email" class="field" placeholder="نام ...">
-                    <i class="fa fa-user-plus field_icon"></i>
-                </div>
-                <div class="mt-3 position-relative">
-                    <input type="email" class="field" placeholder="ایمیل ...">
-                    <i class="fa fa-envelope field_icon" aria-hidden="true"></i>
-                </div>
-                <div class="mt-3 position-relative">
-                    <input type="password" class="field" id="fieldPass" placeholder="رمز عبور ...">
-                    <i class="fa fa-lock field_icon"></i>
-                    <button type="button" id="showPass"></button>
-                </div>
-                <div class="mt-3 position-relative">
-                    <input type="password" class="field" id="fieldPass" placeholder="تکرار رمز عبور ...">
-                    <i class="fa fa-check field_icon"></i>
-                    <button type="button" id="showPass"></button>
-                </div>
-                <div class="mt-3">
-                    <button type="submit" class="btn-submit bg-primary">
-                        <i class="fa fa-user-plus ms-1"></i>
-                        <span>ثبت نام</span>
-                    </button>
-                </div>
-        
-                <p class="text">
-                    قبلا ثبت نام کرده اید ؟ <a href="/login.php" class="text-primary">ورود</a>
-                </p>
-            </form>
-        </div>
-    </section>
+<section class="d-flex justify-content-center align-items-center min-h-screen bg">
+    <div id="overlay"></div>
+    <div class="form-container">
+        <form action="#" method="post">
+            <span class="text-bg-danger"><?php $error ?></span>
+            <h1 class="title">ثبت نام در وبلاگ</h1>
+            <div class="mt-3 position-relative">
+                <input type="" name="username" class="field" placeholder="نام ...">
+                <i class="fa fa-user-plus field_icon"></i>
+            </div>
+            <div class="mt-3 position-relative">
+                <input type="email" name="email" class="field" placeholder="ایمیل ...">
+                <i class="fa fa-envelope field_icon" aria-hidden="true"></i>
+            </div>
+            <div class="mt-3 position-relative">
+                <input type="password" name="password" class="field" id="fieldPass" placeholder="رمز عبور ...">
+                <i class="fa fa-lock field_icon"></i>
+                <button type="button" id="showPass"></button>
+            </div>
+            <div class="mt-3 position-relative">
+                <input type="password" name="repassword" class="field" id="fieldPass" placeholder="تکرار رمز عبور ...">
+                <i class="fa fa-check field_icon"></i>
+                <button type="button" id="showPass"></button>
+            </div>
+            <div class="mt-3">
+                <button name="sub" type="submit" class="btn-submit bg-primary">
+                    <i class="fa fa-user-plus ms-1"></i>
+                    <span>ثبت نام</span>
+                </button>
+            </div>
 
-    <script src="js/showPassword.js"></script>
-    <script src="js/darkMode.js"></script>
-    <script src="js/scroll.js"></script>
+            <p class="text">
+                قبلا ثبت نام کرده اید ؟ <a href="/login.php" class="text-primary">ورود</a>
+            </p>
+        </form>
+    </div>
+</section>
+
+<script src="js/showPassword.js"></script>
+<script src="js/darkMode.js"></script>
+<script src="js/scroll.js"></script>
 </body>
 </html>
