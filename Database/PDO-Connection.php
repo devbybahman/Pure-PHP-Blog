fcb837464cea2a2d01f5a6d9117426ed2e96093d
BAHMAN <bahmansnights@gmail.com>
2025-07-14 15:38:02 +0430

    PDO-Connection to database

--- Database/PDO-Connection.php
+++ Database/PDO-Connection.php
@@ -1,1 +1,13 @@
 ﻿<?php
+
+try {
+    $connection=new PDO("mysql:host=localhost;dbname=codeyad", "root", "");
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+
+
+}
+catch (PDOException $e) {
+    echo "Connection Error: "  . $e->getMessage();
+}
+?>
