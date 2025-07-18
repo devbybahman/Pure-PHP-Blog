079342664fdcc301a973d30098540ff1d4abb07c
BAHMAN <bahmansnights@gmail.com>
2025-07-18 11:41:10 +0430

    add view counter

--- single.php
+++ single.php
@@ -1,15 +1,28 @@
 <?php
 require "Database/PDO-Connection.php";
-$id=$_GET['id'];
-$select=$connection->prepare("SELECT * FROM autors");
+$id = $_GET['id'];
+$select = $connection->prepare("SELECT * FROM autors");
 $select->execute();
-$authors=$select->fetchAll(PDO::FETCH_ASSOC);
-$selectposts=$connection->prepare("SELECT * FROM posts WHERE id=?");
-$selectposts->bindValue(1,$id);
+$authors = $select->fetchAll(PDO::FETCH_ASSOC);
+$selectposts = $connection->prepare("SELECT * FROM posts WHERE id=?");
+$selectposts->bindValue(1, $id);
 $selectposts->execute();
-$post=$selectposts->fetch(pdo::FETCH_ASSOC);
-?>
+$post = $selectposts->fetch(pdo::FETCH_ASSOC);
 
+
+$views = $connection->prepare("INSERT INTO views SET view_post=?");
+$views->bindValue(1, $id);
+$views->execute();
+
+$counteree = $connection->prepare("SELECT COUNT(*) as total FROM views WHERE view_post=?");
+$counteree->bindValue(1, $id);
+$counteree->execute();
+
+$counterVIEWS = $counteree->fetch(PDO::FETCH_ASSOC);
+$counterVIEWSs = $counterVIEWS['total'];
+
+
+?>
 
 
 <!DOCTYPE html>
@@ -32,119 +45,127 @@ $post=$selectposts->fetch(pdo::FETCH_ASSOC);
     <!-- Vazir Font -->
     <link rel="stylesheet" href="fonts/vazir.css">
     <!-- Fontawsome CDN -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css"
+          integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g=="
+          crossorigin="anonymous" referrerpolicy="no-referrer"/>
     <title>پست</title>
 </head>
 <body>
-    <div class="modal fade" id="modalSearchBox">
-        <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-                <form action="#" class="position-relative">
-                    <input type="search" placeholder="جستجو ..." class="form-control searchField">
-                    <button class="searchBtn"><i class="fas fa-search fs-6"></i></button>
-                </form>
-            </div>
+<div class="modal fade" id="modalSearchBox">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <form action="#" class="position-relative">
+                <input type="search" placeholder="جستجو ..." class="form-control searchField">
+                <button class="searchBtn"><i class="fas fa-search fs-6"></i></button>
+            </form>
         </div>
     </div>
+</div>
 
 
-    <nav class="navMenu navbar navbar-dark navbar-expand-lg align-items-center bg-primary fixed-top">
-        <div class="container flex-row-reverse">
-            <div class="d-flex align-items-center">
-                <button type="button" class="search-icon" data-bs-toggle="modal" data-bs-target="#modalSearchBox">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-search" viewBox="0 0 16 16">
-                        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-                    </svg>
-                </button>
-                <button id="switchTheme"></button>
-                <a class="navbar-brand text-white fw-bold fs-5" href="/index.php"><img src="https://codeyad.com/assets/images/logo.png?v=LeGU9ZpNcH1zdFN4EVqXRwoS_Iaehq3X46AqXt2uWPk" alt="Codeyad"></a>
-            </div>
-
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
-                <i class="fas fa-bars fs-3"></i>
-            </button>
-
-
-            <div class="collapse navbar-collapse right-nav justify-content-start" id="navbar">
-                <ul class="navbar-nav nav-left">
-                    <li class="nav-item me-0">
-                        <a class="nav-link mt-3 mt-lg-0" href="/index.php">
-                            <i class="fa fa-home" aria-hidden="true"></i>
-                            <span>خانه</span>
-                        </a>
-                    </li>
-                    <li class="nav-item me-0">
-                        <a class="nav-link mt-3 mt-lg-0" href="/posts.php">
-                            <i class="fas fa-list"></i>
-                            <span>پست ها</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-item me-0">
-                        <a class="nav-link mt-3 mt-lg-0" href="/login.php">
-                            <i class="fa fa-sign-in ms-1"></i>
-                            <span>ورود</span>
-                        </a>
-                    </li>
-
-                    <li class="nav-item me-0">
-                        <a class="nav-link mt-3 mt-lg-0" href="/register.php">
-                            <i class="fa fa-user-plus ms-1"></i>
-                            <span>عضویت</span>
-                        </a>
-                    </li>
-                </ul>
-            </div>
-
-
-        </div>
-    </nav>
-
-
-    <main style="margin-top: 10rem; margin-bottom: 5rem;">
-        <div class="post-container w-100 mx-auto">
-            <div class="content bg-white">
-                <h4 class="title"><?=$post['title']?></h4>
-
-                <span class="date">نوشته شده توسط
-                    <?php
-               foreach ($authors as $authorss) {
-                   if ($post['author'] == $authorss['id']) {
-                       echo $authorss['username'];
-                       $aimage = $authorss['image'];
-                   }
-
-               } ?>
-                </span>
-<!--                <img src="--><?php //=$aimage?><!--"-->
-                <span class="author"><?=$post['date'] ?></span>
-
-                <div class="img w-100">
-                    <img src="<?=$post['image'] ?>" alt="Image" class="w-100 rounded">
-                </div>
-                <p class="desc">
-                    <?=$post['description'] ?>
-                </p>
-            </div>
-        </div>
-    </main>
-
-
-    <footer class="footer">
-        <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
-            <p class="fw-bold text-white mb-3 mb-md-0 fs-6">تمامی حقوق برای کدیاد محفوظ می باشد &copy;</p>
-            <button type="button" id="scrollUpBtn">
-                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-arrow-up-circle" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+<nav class="navMenu navbar navbar-dark navbar-expand-lg align-items-center bg-primary fixed-top">
+    <div class="container flex-row-reverse">
+        <div class="d-flex align-items-center">
+            <button type="button" class="search-icon" data-bs-toggle="modal" data-bs-target="#modalSearchBox">
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-search"
+                     viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                 </svg>
             </button>
+            <button id="switchTheme"></button>
+            <a class="navbar-brand text-white fw-bold fs-5" href="/index.php"><img
+                        src="https://codeyad.com/assets/images/logo.png?v=LeGU9ZpNcH1zdFN4EVqXRwoS_Iaehq3X46AqXt2uWPk"
+                        alt="Codeyad"></a>
         </div>
-    </footer>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar">
+            <i class="fas fa-bars fs-3"></i>
+        </button>
 
 
+        <div class="collapse navbar-collapse right-nav justify-content-start" id="navbar">
+            <ul class="navbar-nav nav-left">
+                <li class="nav-item me-0">
+                    <a class="nav-link mt-3 mt-lg-0" href="/index.php">
+                        <i class="fa fa-home" aria-hidden="true"></i>
+                        <span>خانه</span>
+                    </a>
+                </li>
+                <li class="nav-item me-0">
+                    <a class="nav-link mt-3 mt-lg-0" href="/posts.php">
+                        <i class="fas fa-list"></i>
+                        <span>پست ها</span>
+                    </a>
+                </li>
 
-    <script src="js/bootstrap.bundle.js"></script>
-    <script src="js/scrollToUp.js"></script>
-    <script src="js/darkMode.js"></script>
+                <li class="nav-item me-0">
+                    <a class="nav-link mt-3 mt-lg-0" href="/login.php">
+                        <i class="fa fa-sign-in ms-1"></i>
+                        <span>ورود</span>
+                    </a>
+                </li>
+
+                <li class="nav-item me-0">
+                    <a class="nav-link mt-3 mt-lg-0" href="/register.php">
+                        <i class="fa fa-user-plus ms-1"></i>
+                        <span>عضویت</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+
+    </div>
+</nav>
+
+
+<main style="margin-top: 10rem; margin-bottom: 5rem;">
+    <div class="post-container w-100 mx-auto">
+        <div class="content bg-white">
+            <h4 class="title"><?= $post['title'] ?></h4>
+
+            <span class="date">نوشته شده توسط
+                    <?php
+                    foreach ($authors as $authorss) {
+                        if ($post['author'] == $authorss['id']) {
+                            echo $authorss['username'];
+                            $aimage = $authorss['image'];
+                        }
+
+                    } ?>
+                </span>
+            <!--                <img src="--><?php //=$aimage?><!--"-->
+            <span class="author"><?= $post['date'] ?></span>
+            <span class="author"><?PHP echo "تعداد بازدیدها: " . $counterVIEWSs;
+                ?></span>
+
+            <div class="img w-100">
+                <img src="<?= $post['image'] ?>" alt="Image" class="w-100 rounded">
+            </div>
+            <p class="desc">
+                <?= $post['description'] ?>
+            </p>
+        </div>
+    </div>
+</main>
+
+
+<footer class="footer">
+    <div class="container d-flex flex-column flex-md-row justify-content-between align-items-center">
+        <p class="fw-bold text-white mb-3 mb-md-0 fs-6">تمامی حقوق برای کدیاد محفوظ می باشد &copy;</p>
+        <button type="button" id="scrollUpBtn">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#fff" class="bi bi-arrow-up-circle"
+                 viewBox="0 0 16 16">
+                <path fill-rule="evenodd"
+                      d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-7.5 3.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V11.5z"/>
+            </svg>
+        </button>
+    </div>
+</footer>
+
+
+<script src="js/bootstrap.bundle.js"></script>
+<script src="js/scrollToUp.js"></script>
+<script src="js/darkMode.js"></script>
 </body>
 </html>
